a8aabed5be52e0a171da2876c755ca8ad92b55f6
g.shchetinkin <g.shchetinkin@connectflexi.com>
2020-03-15 14:41:06 +0300

    Final

--- operation_08_matrix.py
+++ operation_08_matrix.py
@@ -1,6 +1,8 @@
 # %matplotlib inline
 import numpy as np                  #для работы с массивами - их по умолчанию в Python нет
-
+import scipy
+from scipy import linalg
+#1
 a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [0, -1, 12]], float)     #создание матрицы
 print(a.shape)                                                          #размерность матрицы
 b = np.array([[1, 2, 3]], float)                                        #создание вектора
@@ -33,3 +35,21 @@ print(p)
 aa = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 np.linalg.det(aa)                                #определитель
 np.linalg.matrix_rank(b, 0.0001)                 #ранк матрицы
+
+#2 СЛАУ
+print('##########')
+A = np.array([[3, 2], [3, -4]])
+B = np.array([4, 1])
+np.linalg.solve(A, B)                           # решение СЛАУ
+
+P, L, U =  linalg.lu(A)                         # LU разложение СЛА
+print(P, L, U)
+
+A = np.array([[1, 2, -1], [3, -4, 0], [8,-5, 2], [2,0, -5], [11, 4, -7]])
+B = np.array([1, 7, 12, 7, 15])
+X, q, rank, p = np.linalg.lstsq(A, B)           # решение по метода наименьшиъ квадраов
+
+#!!! Не проверенно
+L =  scipy.linalg.choletsky(A)                  #разложение Хлецкого - для симметричных матриц
+Q, R =  np.linalg.qr(A)                         #QR разложение
+np.concatenate(A, B)                            # склеивание матриц
